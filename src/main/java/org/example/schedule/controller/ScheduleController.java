05dc5793ed48c7fcbcf27d24aaaa6fab2f1aa44a
DaHyun-8509 <luce0359@gmail.com>
2025-05-14 10:50:14 +0900

    수정기능 추가

--- src/main/java/org/example/schedule/controller/ScheduleController.java
+++ src/main/java/org/example/schedule/controller/ScheduleController.java
@@ -35,9 +35,6 @@ public class ScheduleController {
     //전체 조회 기능
     @GetMapping
     public List<ScheduleResponseDto> findAllSchedules() {
-        if (scheduleList.isEmpty()) {
-            return Collections.emptyList();
-        }
         List<Schedule> sortedList = new ArrayList<>(scheduleList.values());
         // 날짜 기준 내림차순 정렬
         sortedList.sort(Comparator.comparing(Schedule::getDate).reversed());
@@ -47,5 +44,26 @@ public class ScheduleController {
             responseList.add(new ScheduleResponseDto(schedule));
         }
         return responseList;
+    }
+
+    @PutMapping("/{id}")
+    public ScheduleResponseDto updateSchedule(@PathVariable Long id,
+                                              @RequestParam String password,
+                                              @RequestBody ScheduleRequestDto dto) {
+
+        Schedule schedule = scheduleList.get(id);
+
+        if (schedule == null) {
+            throw new IllegalArgumentException("존재하지 않는 일정입니다.");
+        }
+
+        if (!schedule.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 할일/작성자만 수정하고 수정일은 update 메서드에서 처리
+        schedule.update(dto);
+
+        return new ScheduleResponseDto(schedule);
     }
 }
--- src/main/java/org/example/schedule/entity/Schedule.java
+++ src/main/java/org/example/schedule/entity/Schedule.java
@@ -2,13 +2,32 @@ package org.example.schedule.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import org.example.schedule.dto.ScheduleRequestDto;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class Schedule {
     private Long id;
     private String title;        // 할일
     private String username;     // 작성자명
     private String password;     // 비밀번호
-    private String date;         // 작성일/수정일 (YYYY-MM-DD HH:mm:ss 형태의 문자열로 처리)
+    private String date;
+
+    private String modifiedDate; // 수정일 (YYYY-MM-DD)
+
+    public Schedule(Long id, String title, String username, String password, String date) {
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.password = password;
+        this.date = date;
+    }
+
+    // 수정 기능
+    public void update(ScheduleRequestDto dto) {
+        this.title = dto.getTitle();
+        this.username = dto.getUsername();
+        this.date = java.time.LocalDate.now().toString();
+    }
 }
