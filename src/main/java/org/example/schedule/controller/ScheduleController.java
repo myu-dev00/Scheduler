bf1c027361a29e9d445dc12262d1528992063696
myu <myu.dev00@gmail.com>
2025-05-15 17:05:46 +0900

    Add files via upload

--- src/main/java/org/example/schedule/controller/ScheduleController.java
+++ src/main/java/org/example/schedule/controller/ScheduleController.java
@@ -17,35 +17,30 @@ public class ScheduleController {
         this.scheduleService = scheduleService;
     }
 
-    // 일정 생성
     @PostMapping
     public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto dto) {
         return scheduleService.createSchedule(dto);
     }
 
-    // 전체 일정 조회
     @GetMapping
     public List<ScheduleResponseDto> findAllSchedules() {
         return scheduleService.findAllSchedules();
     }
 
-    // 단건 일정 조회
     @GetMapping("/{id}")
     public ScheduleResponseDto findScheduleById(@PathVariable Long id) {
         return scheduleService.findScheduleById(id);
     }
 
-    // 일정 수정
     @PutMapping("/{id}")
     public ScheduleResponseDto updateSchedule(@PathVariable Long id,
-                                              @RequestParam String contents,
-                                              @RequestParam String username) {
-        return scheduleService.updateSchedule(id, contents, username);
+                                              @RequestParam String password,
+                                              @RequestBody ScheduleRequestDto dto) {
+        return scheduleService.updateSchedule(id, dto, password);
     }
 
-    // 일정 삭제
     @DeleteMapping("/{id}")
-    public void deleteSchedule(@PathVariable Long id) {
-        scheduleService.deleteSchedule(id);
+    public void deleteSchedule(@PathVariable Long id, @RequestParam String password) {
+        scheduleService.deleteSchedule(id, password);
     }
 }
